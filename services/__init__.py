8279cc13f97de072e9a658481b08f33a44983ee7
AlexGonRod <alexgonrod83@gmail.com>
2026-05-19 21:23:11 +0200

    feat: delete unused methods

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,4 @@
 from .scrape_utils import (fill_form, extract_data, extract_table)
-from .utils import (save_data_to_json, format_date)
+from .utils import (save_data_to_json)
 
-__all__ = ["fill_form", "extract_data", "extract_table", "save_data_to_json", "format_date"]
+__all__ = ["fill_form", "extract_data", "extract_table", "save_data_to_json"]
--- services/utils.py
+++ services/utils.py
@@ -1,34 +1,29 @@
-import asyncio
+import inspect
 import json
 import functools
-from datetime import datetime
+
 
 def save_data_to_json(fn):
-    if asyncio.iscoroutinefunction(fn):
+    if inspect.iscoroutinefunction(fn):
+
         @functools.wraps(fn)
         async def async_wrapper(self, *args, **kwargs):
             data = await fn(self, *args, **kwargs)
-            filename = getattr(self, 'filename', "form_results.json")
+            filename = getattr(self, "filename", "form_results.json")
             with open(filename, "w", encoding="utf-8") as f:
                 json.dump(data, f, ensure_ascii=False, indent=2)
             print(f"Datos guardados en {filename}")
             return data
+
         return async_wrapper
 
     @functools.wraps(fn)
     def wrapper(self, *args, **kwargs):
         data = fn(self, *args, **kwargs)
-        filename = getattr(self, 'filename', "form_results.json")
+        filename = getattr(self, "filename", "form_results.json")
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
         print(f"Datos guardados en {filename}")
         return data
-    return wrapper
 
-def format_date(date_str: str) -> str:
-    try:
-        date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-        return date_obj.strftime("%d/%m/%Y")
-    except ValueError as e:
-        print(f"Error parsing date: {e}")
-        return date_str
+    return wrapper
